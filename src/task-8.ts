ba0df91a427688a6f256e5f205324b223752e0b9
Dasha <licht@MacBook-Air-Dasha.local>
2026-02-18 20:26:43 +0100

    feat: initialize TypeScript project with basic examples

---
+++ src/task-8.ts
@@ -0,0 +1,15 @@
+import axios from "axios";
+
+interface Post{
+  id: number;
+  title: string;
+  body: string;
+}
+
+async function fetchPosts(): Promise<Post[]> {
+  const response = await axios.get<Post[]>(
+    'https://jsonplaceholder.typicode.com/posts'
+  );
+  return response.data;
+}
+fetchPosts().then((posts => posts.forEach(post=> console.log(post))));
